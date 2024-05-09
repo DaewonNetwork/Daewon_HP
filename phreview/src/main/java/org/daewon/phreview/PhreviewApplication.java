0639be4ed5f3ad873e98ccce6101d340c2b6e3ce
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-05-09 16:05:18 +0900

    create project

---
+++ phreview/src/main/java/org/daewon/phreview/PhreviewApplication.java
@@ -0,0 +1,13 @@
+package org.daewon.phreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhreviewApplication.class, args);
+	}
+
+}
