1c9f1bfa9a5188431a1779fdd041ba2a9f8a63ab
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-05-20 13:06:39 +0900

    sa

--- phreview/src/test/java/org/daewon/phreview/repository/PharmacyRepositoryTests.java
+++ phreview/src/test/java/org/daewon/phreview/repository/PharmacyRepositoryTests.java
@@ -3,6 +3,7 @@ package org.daewon.phreview.repository;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.daewon.phreview.domain.Pharmacy;
+import org.daewon.phreview.dto.PharmacyDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,10 +19,10 @@ import java.io.IOException;
 import java.sql.Time;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
 @Log4j2
 public class PharmacyRepositoryTests {
-    
 }
