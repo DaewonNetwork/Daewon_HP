6d3af7b52f344c66c3b6408d492647f7f97134eb
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-05-20 10:40:51 +0900

    save

--- phreview/src/main/java/org/daewon/phreview/controller/AuthController.java
+++ phreview/src/main/java/org/daewon/phreview/controller/AuthController.java
@@ -6,16 +6,24 @@ import lombok.extern.log4j.Log4j2;
 import org.daewon.phreview.dto.AuthSignupDTO;
 import org.daewon.phreview.service.AuthService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 @Controller
 @Log4j2
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService userService;
+
+    @GetMapping("/signup")
+    public void signupGET() {
+        log.info("join get.....");
+    }
+    
 
     @Operation(summary = "회원가입 처리", description = "회원가입 요청을 처리합니다.")
     @PostMapping("/signup")
