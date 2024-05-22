5f7b5e5a34e16a807eae85aaf9cd9215e05c1178
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-05-22 12:15:49 +0900

    ll

--- phreview/src/main/java/org/daewon/phreview/controller/AuthController.java
+++ phreview/src/main/java/org/daewon/phreview/controller/AuthController.java
@@ -7,10 +7,7 @@ import org.daewon.phreview.domain.Users;
 import org.daewon.phreview.dto.AuthSignupDTO;
 import org.daewon.phreview.service.AuthService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -29,14 +26,13 @@ public class AuthController {
 
     @Operation(summary = "회원가입 처리", description = "회원가입 요청을 처리합니다.")
     @PostMapping("/signup")
-    public Users signup(@RequestBody AuthSignupDTO authSignupDTO, RedirectAttributes redirectAttributes) {
+    public String signup(@RequestBody AuthSignupDTO authSignupDTO, RedirectAttributes redirectAttributes) {
         log.info(authSignupDTO);
         try {
             Users user = authService.signup(authSignupDTO);
-            return user;
         } catch (AuthService.MidExistException e) {
             redirectAttributes.addFlashAttribute("error", "email");
-            e.
+            return "error";
         }
     }
 }
