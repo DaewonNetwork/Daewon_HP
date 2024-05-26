494c54d5ce2ce85e35eaf4929cce12170e7963ae
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-05-26 13:51:41 +0900

    save

--- client/src/app/(routes)/(app)/page.tsx
+++ client/src/app/(routes)/(app)/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "@/(FSD)/shareds/styles/HomeStyle.module.scss";
 import MapSearchForm from "@/(FSD)/features/map/ui/MapSearchForm";
+import SlideCitySelectBar from "@/(FSD)/widgets/map/ui/SlideCitySelectBar";
 
 const Page = () => {
     return (
         <div className={styles.container}>
             <MapSearchForm />
+            <SlideCitySelectBar />
         </div>
     )
 }
