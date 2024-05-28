f33d922af135ee1d14ba6858c59711ad5df5e7d1
mingi502 <116711211+mingi502@users.noreply.github.com>
2024-05-28 12:02:46 +0900

    re

--- phreview/src/main/java/org/daewon/phreview/service/AuthService.java
+++ phreview/src/main/java/org/daewon/phreview/service/AuthService.java
@@ -3,6 +3,8 @@ package org.daewon.phreview.service;
 import org.daewon.phreview.domain.Users;
 import org.daewon.phreview.dto.AuthSignupDTO;
 
+import java.awt.*;
+
 public interface AuthService {
     static class MidExistException extends Exception {
 
@@ -14,6 +16,14 @@ public interface AuthService {
 
     Users signup(AuthSignupDTO authSignupDTO) throws MidExistException;
 
-    
+    // 이미지 업로드 메서드
+    void uploadImage(String userId, byte[] imageData) throws ImageUploadException;
+
+    static class ImageUploadException extends Exception {
+        public ImageUploadException() {}
+        public ImageUploadException(String msg) {
+            super(msg);
+        }
+    }
 
 }
