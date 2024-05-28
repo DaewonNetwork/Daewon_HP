d8a8e01e14be0ba4d2ae9518930b8ef65a0a9dde
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-05-28 16:19:20 +0900

    asd

--- phreview/src/main/java/org/daewon/phreview/service/PharmacyService.java
+++ phreview/src/main/java/org/daewon/phreview/service/PharmacyService.java
@@ -1,20 +1,16 @@
 package org.daewon.phreview.service;
 
 import org.daewon.phreview.domain.EnjoyPh;
-import org.daewon.phreview.dto.EnjoyPhDTO;
-import org.daewon.phreview.dto.PharmacyDTO;
-import org.daewon.phreview.dto.ReplyDTO;
+import org.daewon.phreview.dto.*;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface PharmacyService {
 
-    List<PharmacyDTO> regionCategorySearch(String phAdd);
-    List<PharmacyDTO> nearSearch(double lat, double lng);
-//    List<PharmacyDTO> NameSearch(String keyword);
-    List<PharmacyDTO> NameOrAddSearch(String keyword);
-    List<PharmacyDTO> NameSearchInCity(String keyword, String city);
+     PageResponseDTO<PharmacyDTO> regionCategorySearch(String city, PageRequestDTO pageRequestDTO);
+     PageResponseDTO<PharmacyDTO> nearSearch(double lat, double lng, PageRequestDTO pageRequestDTO);
+//     PageResponseDTO<PharmacyDTO> NameSearch(String keyword);
+     PageResponseDTO<PharmacyDTO> NameOrAddSearch(String keyword, PageRequestDTO pageRequestDTO);
+     PageResponseDTO<PharmacyDTO> NameSearchInCity(String keyword, String city, PageRequestDTO pageRequestDTO);
 
     PharmacyDTO getPharmacyInfo(Long phId);
 
--- phreview/src/main/java/org/daewon/phreview/service/PharmacyServiceImpl.java
+++ phreview/src/main/java/org/daewon/phreview/service/PharmacyServiceImpl.java
@@ -9,11 +9,17 @@ import org.daewon.phreview.domain.Pharmacy;
 import org.daewon.phreview.domain.Reply;
 import org.daewon.phreview.domain.Users;
 import org.daewon.phreview.dto.EnjoyPhDTO;
+import org.daewon.phreview.dto.PageRequestDTO;
+import org.daewon.phreview.dto.PageResponseDTO;
 import org.daewon.phreview.dto.PharmacyDTO;
 import org.daewon.phreview.repository.EnjoyRepository;
 import org.daewon.phreview.repository.PharmacyRepository;
 import org.daewon.phreview.repository.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -21,6 +27,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Log4j2
@@ -33,68 +40,68 @@ public class PharmacyServiceImpl implements PharmacyService {
     private final EnjoyRepository enjoyRepository;
 
 
-    public List<PharmacyDTO> regionCategorySearch(String city) {
-        List<Pharmacy> result = pharmacyRepository.findByCity(city);
-        List<PharmacyDTO> pharmacyDTOList = new ArrayList<>();
-        for(Pharmacy p : result){
-            PharmacyDTO dto = PharmacyDTO.builder()
-                    .phId(p.getPhId())
-                    .phName(p.getPhName())
-                    .phAdd(p.getPhAdd())
-                    .phTel(p.getPhTel())
-                    .phX(p.getPhX())
-                    .phY(p.getPhY())
-                    .enjoyIndex(p.getEnjoyIndex())
-                    .starIndex(p.getStarIndex())
-                    .timeHoliEndDate(p.getTimeHoliEndDate())
-                    .timeHoliStartDate(p.getTimeHoliStartDate())
-                    .timeSatEndDate(p.getTimeSatEndDate())
-                    .timeSatStartDate(p.getTimeSatStartDate())
-                    .timeWeekEndDate(p.getTimeWeekEndDate())
-                    .timeWeekStartDate(p.getTimeWeekStartDate())
-                    .build();
-            pharmacyDTOList.add(dto);
+    public PageResponseDTO<PharmacyDTO> regionCategorySearch(String city, PageRequestDTO pageRequestDTO) {
+        Pageable pageable = PageRequest.of(
+                pageRequestDTO.getPage() <= 0 ? 0: pageRequestDTO.getPage() - 1,
+                pageRequestDTO.getSize(),
+                Sort.by("phId").ascending());
+        Page<Pharmacy> result = pharmacyRepository.findByCity(city,pageable);
+        log.info(result);
 
-        }
-        return pharmacyDTOList;
+        List<PharmacyDTO> dtoList = result.getContent().stream().map(pharmacy -> {
+            PharmacyDTO pharmacyDTO = modelMapper.map(pharmacy, PharmacyDTO.class);
+            return pharmacyDTO;
+        }).collect(Collectors.toList());
+        log.info(dtoList);
+        return PageResponseDTO.<PharmacyDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int)result.getTotalElements())
+                .build();
     }
+
+//    @Override
+//    public PageResponseDTO<PharmacyDTO> nearSearch(double lat, double lng, PageRequestDTO pageRequestDTO) {
+//        Pageable pageable = PageRequest.of(
+//                pageRequestDTO.getPage() <= 0 ? 0: pageRequestDTO.getPage() - 1,
+//                pageRequestDTO.getSize(),
+//                Sort.by("phId").ascending());
+//        Page<PharmacyDTO> result = pharmacyRepository.findByLoc(lat,lng,pageable);
+//        // log.info(result); // 필요한 경우에만 로그를 남기는 것이 좋습니다.
+//
+//        return PageResponseDTO.<PharmacyDTO>withAll()
+//                .pageRequestDTO(pageRequestDTO)
+//                .dtoList(result.getContent()) // 변경된 부분
+//                .total((int)result.getTotalElements())
+//                .build();
+//    }
 
     @Override
-    public List<PharmacyDTO> nearSearch(double lat, double lng) {
-        List<Pharmacy> result = pharmacyRepository.findByLoc(lat,lng);
-
-        List<PharmacyDTO> pharmacyDTOList = new ArrayList<>();
-        for(Pharmacy p : result){
-            PharmacyDTO dto = PharmacyDTO.builder()
-                    .phId(p.getPhId())
-                    .phName(p.getPhName())
-                    .phAdd(p.getPhAdd())
-                    .phTel(p.getPhTel())
-                    .phX(p.getPhX())
-                    .phY(p.getPhY())
-                    .enjoyIndex(p.getEnjoyIndex())
-                    .starIndex(p.getStarIndex())
-                    .timeHoliEndDate(p.getTimeHoliEndDate())
-                    .timeHoliStartDate(p.getTimeHoliStartDate())
-                    .timeSatEndDate(p.getTimeSatEndDate())
-                    .timeSatStartDate(p.getTimeSatStartDate())
-                    .timeWeekEndDate(p.getTimeWeekEndDate())
-                    .timeWeekStartDate(p.getTimeWeekStartDate())
-                    .build();
-            pharmacyDTOList.add(dto);
-        }
-
-
-        return pharmacyDTOList;
+    public PageResponseDTO<PharmacyDTO> nearSearch(double lat, double lng, PageRequestDTO pageRequestDTO) {
+        Pageable pageable = PageRequest.of(
+                pageRequestDTO.getPage() <= 0 ? 0: pageRequestDTO.getPage() - 1,
+                pageRequestDTO.getSize(),
+                Sort.by("phId").ascending());
+        Page<Pharmacy> result = pharmacyRepository.findByLoc(lat,lng,pageable); // 변경된 부분
+        log.info(result);
+        List<PharmacyDTO> dtoList = result.getContent().stream().map(pharmacy -> {
+            PharmacyDTO pharmacyDTO = modelMapper.map(pharmacy, PharmacyDTO.class);
+            return pharmacyDTO;
+        }).collect(Collectors.toList());
+        log.info(dtoList);
+        return PageResponseDTO.<PharmacyDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int)result.getTotalElements())
+                .build();
     }
-
 
 
 //    @Override
-//    public List<PharmacyDTO> NameSearch(String keyword) {
+//    public PageResponseDTO<PharmacyDTO> NameSearch(String keyword) {
 //        List<Pharmacy> result = pharmacyRepository.findNameByKeyword(keyword);
 //        log.info("name");
-//        List<PharmacyDTO> pharmacyDTOList = new ArrayList<>();
+//        PageResponseDTO<PharmacyDTO> pharmacyDTOList = new ArrayList<>();
 //        for(Pharmacy p : result){
 //            PharmacyDTO dto = PharmacyDTO.builder()
 //                    .phId(p.getPhId())
@@ -122,61 +129,45 @@ public class PharmacyServiceImpl implements PharmacyService {
 //    }
 
     @Override
-    public List<PharmacyDTO> NameOrAddSearch(String keyword) {
-        List<Pharmacy> result = pharmacyRepository.findAddOrNameByKeyword(keyword);
-        log.info("nameOrAdd");
-        List<PharmacyDTO> pharmacyDTOList = new ArrayList<>();
-        for(Pharmacy p : result){
-            PharmacyDTO dto = PharmacyDTO.builder()
-                    .phId(p.getPhId())
-                    .phName(p.getPhName())
-                    .phAdd(p.getPhAdd())
-                    .phTel(p.getPhTel())
-                    .phX(p.getPhX())
-                    .phY(p.getPhY())
-                    .enjoyIndex(p.getEnjoyIndex())
-                    .starIndex(p.getStarIndex())
-                    .timeHoliEndDate(p.getTimeHoliEndDate())
-                    .timeHoliStartDate(p.getTimeHoliStartDate())
-                    .timeSatEndDate(p.getTimeSatEndDate())
-                    .timeSatStartDate(p.getTimeSatStartDate())
-                    .timeWeekEndDate(p.getTimeWeekEndDate())
-                    .timeWeekStartDate(p.getTimeWeekStartDate())
-                    .build();
-            pharmacyDTOList.add(dto);
-        }
+    public PageResponseDTO<PharmacyDTO> NameOrAddSearch(String keyword, PageRequestDTO pageRequestDTO) {
+        Pageable pageable = PageRequest.of(
+                pageRequestDTO.getPage() <= 0 ? 0: pageRequestDTO.getPage() - 1,
+                pageRequestDTO.getSize(),
+                Sort.by("phId").ascending());
+        Page<Pharmacy> result = pharmacyRepository.findAddOrNameByKeyword(keyword,pageable);
+        log.info(result);
 
-
-        return pharmacyDTOList;
+        List<PharmacyDTO> dtoList = result.getContent().stream().map(pharmacy -> {
+            PharmacyDTO pharmacyDTO = modelMapper.map(pharmacy, PharmacyDTO.class);
+            return pharmacyDTO;
+        }).collect(Collectors.toList());
+        log.info(dtoList);
+        return PageResponseDTO.<PharmacyDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int)result.getTotalElements())
+                .build();
     }
 
     @Override
-    public List<PharmacyDTO> NameSearchInCity(String city,String keyword) {
-        log.info("nameInCity");
-        List<Pharmacy> result = pharmacyRepository.findNameByCityAndKeyword(keyword, city);
-        List<PharmacyDTO> pharmacyDTOList = new ArrayList<>();
-        for(Pharmacy p : result){
-            PharmacyDTO dto = PharmacyDTO.builder()
-                    .phId(p.getPhId())
-                    .phName(p.getPhName())
-                    .phAdd(p.getPhAdd())
-                    .phTel(p.getPhTel())
-                    .phX(p.getPhX())
-                    .phY(p.getPhY())
-                    .enjoyIndex(p.getEnjoyIndex())
-                    .starIndex(p.getStarIndex())
-                    .timeHoliEndDate(p.getTimeHoliEndDate())
-                    .timeHoliStartDate(p.getTimeHoliStartDate())
-                    .timeSatEndDate(p.getTimeSatEndDate())
-                    .timeSatStartDate(p.getTimeSatStartDate())
-                    .timeWeekEndDate(p.getTimeWeekEndDate())
-                    .timeWeekStartDate(p.getTimeWeekStartDate())
-                    .build();
-            pharmacyDTOList.add(dto); // 4
-        }
+    public PageResponseDTO<PharmacyDTO> NameSearchInCity(String city, String keyword, PageRequestDTO pageRequestDTO) {
+        Pageable pageable = PageRequest.of(
+                pageRequestDTO.getPage() <= 0 ? 0: pageRequestDTO.getPage() - 1,
+                pageRequestDTO.getSize(),
+                Sort.by("phId").ascending());
+        Page<Pharmacy> result = pharmacyRepository.findNameByCityAndKeyword(city,keyword,pageable);
+        log.info(result);
 
-
-        return pharmacyDTOList;
+        List<PharmacyDTO> dtoList = result.getContent().stream().map(pharmacy -> {
+            PharmacyDTO pharmacyDTO = modelMapper.map(pharmacy, PharmacyDTO.class);
+            return pharmacyDTO;
+        }).collect(Collectors.toList());
+        log.info(dtoList);
+        return PageResponseDTO.<PharmacyDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int)result.getTotalElements())
+                .build();
     }
 
     @Override
