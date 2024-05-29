1fc6950c833c77e19764163e5f2d27a754a83b04
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-05-29 11:06:11 +0900

    a

--- phreview/src/main/java/org/daewon/phreview/dto/PageResponseDTO.java
+++ phreview/src/main/java/org/daewon/phreview/dto/PageResponseDTO.java
@@ -14,8 +14,8 @@ public class PageResponseDTO<E> {
     private int size;
     private int totalPageIndex;
     private int totalIndex;
-    private int start;
-    private int end;
+    private int viewStart;
+    private int viewEnd;
     private boolean prev;
     private boolean next;
     private List<E> phList;
@@ -32,10 +32,10 @@ public class PageResponseDTO<E> {
 
         this.phList = phList;
 
-        this.end = Math.min((int)(Math.ceil((double)(pageIndex + 1) / 10.0)) * 10, totalPageIndex); // 화면에 표시할 페이지번호 갯수...
-        this.start = this.end - 9; // 화면에서 시작번호
-        this.prev = this.start > 1;
-        this.next = totalPageIndex > this.end;
+        this.viewEnd = Math.min((int)(Math.ceil((double)(pageIndex + 1) / 10.0)) * 10, totalPageIndex); // 화면에 표시할 페이지번호 갯수...
+        this.viewStart = this.viewEnd - 9; // 화면에서 시작번호
+        this.prev = this.viewStart > 1;
+        this.next = totalPageIndex > this.viewEnd;
     }
 }
 
