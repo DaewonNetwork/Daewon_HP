0881d1b628e79446925effdf17a7b1027a5f90a9
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-05-30 11:46:29 +0900

    asd

--- phreview/src/main/java/org/daewon/phreview/controller/EnjoyController.java
+++ phreview/src/main/java/org/daewon/phreview/controller/EnjoyController.java
@@ -5,9 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.daewon.phreview.domain.PharmacyEnjoy;
 import org.daewon.phreview.dto.EnjoyPhDTO;
-import org.daewon.phreview.dto.PharmacyEnjoyDTO;
 import org.daewon.phreview.repository.PharmacyEnjoyRepository;
-import org.daewon.phreview.security.exception.PharmacyNotFoundException;
 import org.daewon.phreview.service.EnjoyService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Log4j2
@@ -31,9 +30,8 @@ public class EnjoyController {
     @GetMapping("/")
     public int enjoy(@RequestParam Long phId){ // 즐겨찾기
         enjoyService.enjoyPharmacy(phId);
-        PharmacyEnjoy pharmacyEnjoy = pharmacyEnjoyRepository.findById(phId).orElseThrow(() -> new PharmacyNotFoundException(phId));
-        log.info(pharmacyEnjoy.getEnjoyIndex());
-        return pharmacyEnjoy.getEnjoyIndex(); // 즐겨찾기 수 반환
+        Optional<PharmacyEnjoy> pharmacyEnjoy = pharmacyEnjoyRepository.findByPhId(phId);
+        return pharmacyEnjoy.get().getEnjoyIndex(); // 즐겨찾기 수 반환
     }
 
     @Operation(summary = "자신이 즐겨찾기한 약국 목록")
