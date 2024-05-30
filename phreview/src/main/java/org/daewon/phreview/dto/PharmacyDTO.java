df52fd9d2fb1ed51c629399f79716efe2cd48f10
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-05-30 16:30:35 +0900

    asd

--- phreview/src/main/java/org/daewon/phreview/dto/PharmacyDTO.java
+++ phreview/src/main/java/org/daewon/phreview/dto/PharmacyDTO.java
@@ -18,12 +18,6 @@ public class PharmacyDTO {
     private String phName; // 병원 이름
     private String phTel; // 병원 전화번호
     private String phAdd; // 병원 주소
-    private String timeWeekStartDate; // 평일 시작
-    private String timeWeekEndDate; // 평일 종료
-    private String timeSatStartDate; // 토요일 시작
-    private String timeSatEndDate; // 토요일 종료
-    private String timeHoliStartDate; // 공휴일 시작
-    private String timeHoliEndDate; // 공휴일 종료
     private double phX;  // 경도
     private double phY;  // 위도
 
