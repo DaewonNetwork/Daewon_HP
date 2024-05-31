ececcb3b276e44ec4ec4be52d79decb9d36e7178
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-05-31 11:53:11 +0900

    asd

--- phreview/src/main/java/org/daewon/phreview/controller/ReviewPublicController.java
+++ phreview/src/main/java/org/daewon/phreview/controller/ReviewPublicController.java
@@ -4,11 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.daewon.phreview.domain.PharmacyStar;
+import org.daewon.phreview.domain.Review;
 import org.daewon.phreview.dto.PharmacyStarDTO;
 import org.daewon.phreview.dto.ReviewDTO;
 
 import org.daewon.phreview.dto.ReviewReadDTO;
+import org.daewon.phreview.repository.ReviewRepository;
 import org.daewon.phreview.service.LikeService;
+import org.daewon.phreview.service.PharmacyService;
 import org.daewon.phreview.service.ReviewService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,14 +28,23 @@ public class ReviewPublicController {
 
     private final ReviewService reviewService;
     private final LikeService likeService;
+
     @GetMapping()
     public List<ReviewReadDTO> readReviews(@RequestParam(name = "phId") Long phId) {
         List<ReviewReadDTO> reviewList = reviewService.readReview(phId);
         return reviewList;
     }
 
-    @Operation(summary = "좋아요 순")
+    @Operation(summary = "모든 리뷰")
     @GetMapping("/list")
+    public List<ReviewReadDTO> readReviewsByLikeIndexDesc(){ // 좋아요 수가 높은 리뷰 내림차순
+        List<ReviewReadDTO> reviewlist =  reviewService.readAllReview();
+        return reviewlist;
+    }
+
+
+    @Operation(summary = "좋아요 순")
+    @GetMapping("/list/like")
     public List<ReviewReadDTO> readReviewsByLikeIndexDesc(@RequestParam(name = "phId") Long phId){ // 좋아요 수가 높은 리뷰 내림차순
         List<ReviewReadDTO> reviewlist = likeService.getReviewsByLikeIndexDesc(phId);
         return reviewlist;
