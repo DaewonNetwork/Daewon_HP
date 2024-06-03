bf49eb676ed2906f448f65ae51e2b655cdffffd2
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-06-03 17:37:42 +0900

    a

--- phreview/src/main/java/org/daewon/phreview/config/RootConfig.java
+++ phreview/src/main/java/org/daewon/phreview/config/RootConfig.java
@@ -1,7 +1,6 @@
 package org.daewon.phreview.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
