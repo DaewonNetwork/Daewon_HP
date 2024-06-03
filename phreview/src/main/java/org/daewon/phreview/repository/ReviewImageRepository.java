4b807b6e43b7e75a5284b9fb86907fc1f8bf611d
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-06-03 15:23:15 +0900

    asd

--- phreview/src/main/java/org/daewon/phreview/repository/ReviewImageRepository.java
+++ phreview/src/main/java/org/daewon/phreview/repository/ReviewImageRepository.java
@@ -5,7 +5,10 @@ import org.daewon.phreview.domain.ReviewImage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewImageRepository extends JpaRepository<ReviewImage, String> {
-    Page<ReviewImage> findByReview(Review review, Pageable pageable);
+
+    @Query("SELECT r FROM ReviewImage r WHERE r.review.reviewId = :reviewId")
+    ReviewImage findByReviewId(Long reviewId);
 }
