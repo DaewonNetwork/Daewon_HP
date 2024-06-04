c6ee470cdfdefff1f183f9490faec4736af20c87
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-06-04 14:53:31 +0900

    asd

--- phreview/src/main/java/org/daewon/phreview/controller/ReviewController.java
+++ phreview/src/main/java/org/daewon/phreview/controller/ReviewController.java
@@ -154,27 +154,33 @@ public class ReviewController {
         return reviewlist;
     }
 
-    private static final String UPLOAD_FOLDER = "C:\\upload\\"; // 업로드듼 폴더(createReview시 파일 경로)
+    private static final String UPLOAD_FOLDER = "C:\\upload\\"; // 업로드된 폴더(createReview 시 파일 경로)
+
     @Operation(summary = "이미지")
     @GetMapping("/read/image")
-    public ResponseEntity<byte[]> readReviewImage(Long reviewId) throws IOException {
+    public ResponseEntity<byte[]> readReviewImage(Long reviewId) {
+        try {
+            ReviewImage reviewImage = reviewImageRepository.findByReviewId(reviewId).orElse(null);
+            if (reviewImage == null) {
+                log.info("Review Image Not Found");
+            }
+            String uuid = reviewImage.getUuid();
+            String fileName = reviewImage.getFileName();
 
-        ReviewImage reviewImage = reviewImageRepository.findByReviewId(reviewId).orElse(null);
-        if (reviewImage == null) {
-            return ResponseEntity.notFound().build();
+            String filePath = UPLOAD_FOLDER + uuid + "_" + fileName;
+
+            // 파일을 바이트 배열로 읽기
+            Path path = Paths.get(filePath);
+            byte[] image = Files.readAllBytes(path);
+
+            // 응답에 이미지와 Content-Type 설정 후 반환
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+        } catch (IOException e) {
+            log.error("Error reading review image: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        String uuid = reviewImage.getUuid();
-        String fileName = reviewImage.getFileName();
-
-        String filePath = UPLOAD_FOLDER + uuid+"_"+fileName;
-
-        // 파일을 바이트 배열로 읽기
-        Path path = Paths.get(filePath);
-        byte[] image = Files.readAllBytes(path);
-
-        // 응답에 이미지와 Content-Type 설정 후 반환
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
     }
+
 
 
 
