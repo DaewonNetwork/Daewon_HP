6ea7d13157c14db82b270b06201cdee77cf3745a
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-06-04 16:34:55 +0900

    asd

--- client/src/(FSD)/shareds/types/Review.type.ts
+++ client/src/(FSD)/shareds/types/Review.type.ts
@@ -5,28 +5,8 @@ export interface ReviewType {
     userName: string;
     reviewTitle: string;
     reviewText: string;
-    likeIndex: number;
     star: number;
     createAt: string;
     updateAt: string;
     reviewImage: Uint8Array;
 }
-/*
- 
- private Long reviewId;
-    private String phName;
-    private String userName;
-
-    @NotEmpty
-    private String reviewText;
-
-    private int star;
-
-    private int likeIndex;
-    private int replyIndex;
-
-    private LocalDateTime createAt;
-    private LocalDateTime updateAt;
-
-    private List<ReviewImageDTO> reviewImages; // 리뷰 이미지 리스트
-*/
