6b9c9aca443fec60bb72ca91bcd99e8ca7973208
huntingforheat <asktt1234@gmail.com>
2024-06-04 10:59:01 +0900

    g

--- phreview/src/main/java/org/daewon/phreview/repository/EnjoyRepository.java
+++ phreview/src/main/java/org/daewon/phreview/repository/EnjoyRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 // 약국 유저 사이에 관계
 public interface EnjoyRepository extends JpaRepository<EnjoyPh, Long> {
     @Query(value = "SELECT * from enjoy_ph where ph_id=:phId and user_id=:userId",nativeQuery = true)
-    EnjoyPh findByPharmacyAndUsers(Long phId, Long userId); // phId, userId를 통해 EnjoyPh 내 검색
+    EnjoyPh findByPharmacyAndUsers(Long phId, Long userId); // phId, userId를 통해 EnjoyPh(약국 즐겨찾기) 내 검색
 
     List<EnjoyPh> findByUsersUserIdOrderByEnjoyIdDesc(Long userId); // userId를 통해 검색후 EnjoyId가 높은순 내림차순(최신순)
 }
